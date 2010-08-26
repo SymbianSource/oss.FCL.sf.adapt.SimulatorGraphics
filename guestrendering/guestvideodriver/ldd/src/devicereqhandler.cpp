54fc4fdf599944687362798334d6fd996a66d4a1
Faisal Memon <faisal.memon@nokia.com>
2010-08-26 13:24:02 +0100

    Fix copyright notices and license text.
    --HG--
    branch : bug235_bringup_0

--- guestrendering/guestvideodriver/ldd/src/devicereqhandler.cpp
+++ guestrendering/guestvideodriver/ldd/src/devicereqhandler.cpp
@@ -1,24 +1,19 @@
-/*
- * =============================================================================
- *  Name        : devicereqhandler.cpp
- *  Part of     : 
- *  Description : Implementation of the class DDeviceReqHandler.
- *  Version     : %version:6 %
- *
- *  Copyright © 2008-2010 Nokia. All rights reserved.
- *  This material, including documentation and any related
- *  computer programs, is protected by copyright controlled by
- *  Nokia. All rights are reserved. Copying, including
- *  reproducing, storing, adapting or translating, any
- *  or all of this material requires the prior written consent of
- *  Nokia. This material also contains confidential
- *  information which may not be disclosed to others without the
- *  prior written consent of Nokia.
- * =============================================================================
- */
+// Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+// All rights reserved.
+// This component and the accompanying materials are made available
+// under the terms of "Eclipse Public License v1.0"
+// which accompanies this distribution, and is available
+// at the URL "http://www.eclipse.org/legal/epl-v10.html".
+//
+// Initial Contributors:
+// Nokia Corporation - initial contribution.
+//
+// Contributors:
+//
+// Description:
+// Device Request Handler Implementation
 
 
-// INCLUDE FILES
 #include <platform.h>
 #include <assp.h>
 #include <kernel/kernel.h>
--- guestrendering/guestvideodriver/ldd/src/virtualvideohwinterface.cpp
+++ guestrendering/guestvideodriver/ldd/src/virtualvideohwinterface.cpp
@@ -1,21 +1,17 @@
-/*
- * =============================================================================
- *  Name        : virtualvideohwinterface.cpp
- *  Part of     : 
- *  Description : Implementation file for the class DVirtualVideoHwInterface.
- *  Version     : %version:3 %
- *
- *  Copyright © 2002-2009 Nokia. All rights reserved.
- *  This material, including documentation and any related
- *  computer programs, is protected by copyright controlled by
- *  Nokia. All rights are reserved. Copying, including
- *  reproducing, storing, adapting or translating, any
- *  or all of this material requires the prior written consent of
- *  Nokia. This material also contains confidential
- *  information which may not be disclosed to others without the
- *  prior written consent of Nokia.
- * =============================================================================
- */
+// Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+// All rights reserved.
+// This component and the accompanying materials are made available
+// under the terms of "Eclipse Public License v1.0"
+// which accompanies this distribution, and is available
+// at the URL "http://www.eclipse.org/legal/epl-v10.html".
+//
+// Initial Contributors:
+// Nokia Corporation - initial contribution.
+//
+// Contributors:
+//
+// Description:
+// Implementation of Virtual Video Hardware Interface
 
 
 // INCLUDE FILES
@@ -279,6 +275,3 @@ EXPORT_C TPhysAddr  DVirtualVideoHwInterface::GetFrameBase()
 	return 0;
 #endif
 }
-
-
-// End of file
